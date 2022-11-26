54ce02ea4a4f27215d8c24f54d1e2b351f132f5c
djy <dingjiaying@shenniudata.com>
2022-11-26 11:24:54 +0800

    练习

---
+++ testRandom.java
@@ -0,0 +1,12 @@
+class randomTest {
+	public static void main (String[] args){
+		int value = (int)(Math.random()*90 +10);
+		System.out.println(value);
+	}	
+}
+
+class randomTest {
+	public static void main (String[] args){
+		
+	}	
+}
